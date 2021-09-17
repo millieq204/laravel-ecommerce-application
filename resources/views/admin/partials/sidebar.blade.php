dc725c1cdacc51fa800fcfec9480ce5c698f953c
millieq204 <57375328+millieq204@users.noreply.github.com>
2021-09-17 15:19:00 +0100

    sidebar.blade.php updated

---
+++ resources/views/admin/partials/sidebar.blade.php
@@ -0,0 +1,38 @@
+<div class="app-sidebar_overlay" data-toggle="sidebar"></div>
+<aside class="app-sidebar">
+    <div class="app-sidebar_user">
+        <div>
+            <p class="app-sidebar_user-name">John Doe</p>
+            <p class="app-sidebar_user-designation">Frontend Developer</p>
+        </div>
+    </div>
+    <ul class="app-menu">
+        <li>
+            <a class="app-menu_item active" href="#"><i class="app-menu_icon fa fa-dashboard"></i>
+                <span class="app-menu_label">Dashboard</span>
+            </a>
+        </li>
+        <li class="treeview">
+            <a class="app-menu_item" href="#" data-toggle="treeview"><i class="app-menu_icon fa fa-users"></i>
+                <span class="app-menu_label">Users</span>
+                <i class="treeview-indicator fa fa-angle-right"></i>
+            </a>
+            <ul class="treeview-menu">
+                <li>
+                    <a class="treeview-item" href="#"><i class="icon fa fa-circle-o"></i> Admin Users</a>
+                </li>
+                <li>
+                    <a class="treeview-item" href="#" target="_blank" rel="noopener noreffer"><i class="icon fa fa-circle-o"></i> Roles</a>
+                </li>
+                <li>
+                    <a class="treeview-item" href="#"><i class="icon fa fa-circle-o"></i> Permissions</a>
+                </li>
+            </ul>
+        </li>
+        <li>
+            <a class="app-menu_item" href="#"><i class="app-menu_icon fa fa-cogs"></i>
+                <span class="app-menu_label">Settings</span>
+            </a>
+        </li>
+    </ul>
+</aside>
